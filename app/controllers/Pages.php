f7c074660cbbf33b5f66b0dec9d1a5455b75078a
Andreas <a.sutt@khk.ee>
2021-04-07 22:06:13 +0300

    Määratud ja võetud kasutusele kontrolleri meetod ning võimalikud parameetrid

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -11,4 +11,12 @@ class Pages
     {
         echo 'Pages Class is loaded<br>';
     }
+
+    public function index(){
+        echo 'index mehthod is loaded<br>';
+    }
+
+    public function about(){
+        echo 'about method is loaded<br>';
+    }
 }
