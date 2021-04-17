e664d7190a64e638b78ab40ad74e265e6fee16be
Andreas <a.sutt@khk.ee>
2021-04-17 11:57:09 +0300

    Koostatud ja testitud andmete lugemine mitme rea kaupa

--- app/models/Page.php
+++ app/models/Page.php
@@ -15,15 +15,20 @@ class Page
 
     public function addUser(){
         $this->db->query('INSERT INTO users SET name=:name, email=:email, status=:status');
-        $this->db->bind('name', 'user1');
-        $this->db->bind('email', 'user@host.loc');
-        $this->db->bind('status', 'user');
+        $this->db->bind('name', 'user2');
+        $this->db->bind('email', 'user2@host.loc');
+        $this->db->bind('status', 'admin');
         $this->db->execute();
     }
 
     public function getOneUser(){
         $this->db->query('SELECT * FROM users WHERE id=:id');
         $this->db->bind('id', 1);
-        return $this->db->getOne();
+        return  $this->db->getOne();
+    }
+
+    public function getUsers(){
+        $this->db->query('SELECT * FROM users');
+        return $this->db->getAll();
     }
 }
