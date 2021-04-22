e19feb0504f883b39fc583f7aa6a660f9876ca9c
Andreas <a.sutt@khk.ee>
2021-04-22 16:13:57 +0300

    Rakenduse põhi  - raamistik on valmis

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,14 +1,14 @@
 <?php
 // Project name
-define('PROJECT_NAME', 'raamistik');
+define('PROJECT_NAME', 'projectname');
 // App root dir
 define('APPROOT',dirname(dirname( __FILE__)));
 define('URLROOT','http://'.$_SERVER['HTTP_HOST']. '/'.PROJECT_NAME);
 
 
 // db params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'raamistik');
+define('DB_HOST', 'host');
+define('DB_USER', 'user');
+define('DB_PASS', 'pass');
+define('DB_NAME', 'dbname');
 
