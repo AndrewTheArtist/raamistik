d4a4336fd898e793ba68512dfe499f7cc54d3d3d
Andreas <a.sutt@khk.ee>
2021-05-10 10:08:57 +0300

    Loodud kontroller, mudel ja vaade failid kasutajate halduseks

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,6 +1,6 @@
 <?php
 // Project name
-define('PROJECT_NAME', 'projectname');
+define('PROJECT_NAME', 'raamistik');
 // App root dir
 define('APPROOT',dirname(dirname( __FILE__)));
 define('URLROOT','http://'.$_SERVER['HTTP_HOST']. '/'.PROJECT_NAME);
