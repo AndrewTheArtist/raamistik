28efed128d919499400660b44b8b6b31167352a8
Andreas <a.sutt@khk.ee>
2021-05-18 09:44:57 +0300

    Loodud sisse logitud kasutaja navigatsiooni riba

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -2,12 +2,14 @@
     <div class="container-fluid">
         <a class="navbar-brand" href="#">料理本社</a>
         <!--Navbar buttons-->
-
+        <?php if(isset($_SESSION['user_id'])) :?>
+        <?php echo $_SESSION['user_name'];?>
+        <a href="<?php echo URLROOT; ?>/users/logout" class=" mr-3 btn btn-link text-decoration-none text-white ">Log Out</a>
+        <a href="<?php echo URLROOT; ?>/partners/index" class=" mr-3 btn btn-link text-decoration-none text-white ">Firms</a>
+        <?php else:?>
         <a href="<?php echo URLROOT; ?>/users/register" class=" mr-3 btn btn-link text-decoration-none text-white ">Register</a>
         <a href="<?php echo URLROOT; ?>/users/login" class=" mr-3 btn btn-link text-decoration-none text-white ">Login</a>
-        <a href="<?php echo URLROOT; ?>/users/firms" class=" mr-3 btn btn-link text-decoration-none text-white ">Firms</a>
-        <a href="<?php echo URLROOT; ?>/users/contact" class=" mr-3 btn btn-link text-decoration-none text-white ">Contact</a>
-
+        <?php endif;?>
         <!--Navbar buttons end-->
     </div>
 </nav>
